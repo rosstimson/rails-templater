8ddcb23becb804a9386c7efd83c2f97730a12aee
Ross Timson <ross@rosstimson.com>
2010-07-30 19:30:42 +0100

    Fixing conflict with mongoid gem versions.

--- recipes/mongoid.rb
+++ recipes/mongoid.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-gem 'mongoid', '>= 2.0.0.beta11'
-=======
 gem 'mongoid', '>= 2.0.0.beta.14'
->>>>>>> ff5237293fe981abb2352400642484db2a25188d
 gem 'bson_ext', '1.0.4'
 
 stategies << lambda do
